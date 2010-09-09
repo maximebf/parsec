0b11f3b6cc456e2978529322da1f1f187bf334a6
maxime <maxime@maxime-laptop.(none)>
2010-09-09 19:02:12 +0200

    Added CatchAlContext

---
+++ libs/ParseInContext/CatchAllContext.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Harmony
+ * Copyright (c) 2010 Maxime Bouroumeau-Fuseau
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author Maxime Bouroumeau-Fuseau
+ * @copyright 2010 (c) Maxime Bouroumeau-Fuseau
+ * @license http://www.opensource.org/licenses/mit-license.php
+ * @link http://github.com/maximebf
+ */
+ 
+namespace ParseInContext;
+
+abstract class CatchAllContext extends Context
+{
+    protected $_value;
+    
+    public function __call($method, $args)
+    {
+        $this->_value .= $args[0];
+    }
+}
